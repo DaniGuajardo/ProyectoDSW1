b253f08d5342da72e2e1950f1ee7ea1afd2c564b
seikosilvakomesu <seikosilvakomesu@gmail.com>
2023-11-27 01:13:31 -0500

    Metodo Eliminar Categoria

--- src/app/components/category/category-list/category-list.component.ts
+++ src/app/components/category/category-list/category-list.component.ts
@@ -1,20 +1,30 @@
 import { Component } from '@angular/core';
 import { Category } from 'src/app/models/category';
 import { CategoryService } from 'src/app/services/category.service';
-import { CategoryAddComponent } from '../category-add/category-add.component';
 
 @Component({
   selector: 'app-category-list',
   templateUrl: './category-list.component.html',
   styleUrls: ['./category-list.component.css']
 })
-
 export class CategoryListComponent {
-  categories:Category[]=[]
-  constructor(private categoryService:CategoryService) {
-    this.categoryService.getCategories().subscribe(res=>this.categories=res)
+  categories: Category[] = [];
+
+  constructor(private categoryService: CategoryService) {
+    this.categoryService.getCategories().subscribe(res => this.categories = res);
   }
 
-  
-
+  //METODO PARA ELIMIAR
+  deleteCategory(id: number | undefined) {
+    if (id !== undefined) {
+      if (confirm('¿Estás seguro de que quieres eliminar esta categoría?')) {
+        this.categoryService.deleteCategory(id).subscribe(() => {
+          console.log('Categoría eliminada correctamente.');
+          // Recargar la lista de categorías después de la eliminación
+          this.categoryService.getCategories().subscribe(res => this.categories = res);
+        });
+      }
+    }
+  }
 }
+
--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -32,8 +32,10 @@ export class CategoryService {
     return this.httpClient.put<Category>(`${this.url}UpdateCategory`, request)
   }
 
-  deleteCategory(id: number):Observable<boolean>{
-    return this.httpClient.delete<boolean>(`${this.url}DeleteCategory/${id}`)
+ 
+  deleteCategory(id: number): Observable<boolean> {
+  return this.httpClient.delete<boolean>(`${this.url}DeleteCategory?idCategory=${id}`);
   }
+
 
 }
